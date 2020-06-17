395c203727a2b1b255bf61fcde3a4459d219aeb0
Hubert Kaczorowski <h.kaczorowski@stud.elka.pw.edu.pl>
2020-06-17 08:02:50 +0200

    losowanie plansz

--- src/Gra.java
+++ src/Gra.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 public class Gra {
 
     private int poziomTrudnosci;
@@ -9,6 +11,16 @@ public class Gra {
      */
     private int[][] ukladPlanszy;
 
+    private int nrPlanszy;
+
+    public int getNrPlanszy() {
+        return nrPlanszy;
+    }
+
+    public void setNrPlanszy(int nrPlanszy) {
+        this.nrPlanszy = nrPlanszy;
+    }
+
     public int getPoziomTrudnosci() {
         return poziomTrudnosci;
     }
@@ -18,6 +30,7 @@ public class Gra {
     }
 
     public int[][] getUkladPlanszy() {
+
         return ukladPlanszy;
     }
 
@@ -26,16 +39,91 @@ public class Gra {
     }
 
     public void losujUklad(int poziomTrudnosci){
+        Random random = new Random();
+        int numer = random.nextInt(2);
         if (poziomTrudnosci == 1){
-            ukladPlanszy = new int[5][5];
+           if (numer == 0) {
+               int[][] ukladPlanszy =   {{10, 10, 10, 10, 10},
+                                        {10, 10, 10, 1, 10},
+                                        {10, 3, 10, 10, 10},
+                                        {4, 10, 10, 10, 10},
+                                        {10, 10, 10, 10, 2}};
+               nrPlanszy = numer;
+               this.ukladPlanszy = ukladPlanszy;
+           }
+           else if (numer == 1) {
+               int[][] ukladPlanszy = {{1, 10, 10, 3, 10},
+                                        {10, 2, 10, 10, 10},
+                                        {10, 10, 10, 10, 10},
+                                        {10, 10, 10, 10, 10},
+                                        {10, 3, 10, 10, 10}};
+               nrPlanszy = numer;
+               this.ukladPlanszy = ukladPlanszy;
+           }
         }
         else if (poziomTrudnosci == 2){
-            ukladPlanszy = new int[7][7];
+            if (numer == 0){
+                int[][] ukladPlanszy =   {{10, 10, 10, 2, 10, 10, 10},
+                                          {10, 1, 10, 10, 10, 1, 10},
+                                          {10, 10, 10, 10, 10, 10, 10},
+                                          {5, 10, 10, 10, 10, 10, 1},
+                                          {10, 10, 10, 10, 10, 10, 10},
+                                          {10, 1, 10, 10, 10, 1, 10},
+                                          {10, 10, 10, 2, 10, 10, 10}};
+                nrPlanszy = numer+2;
+                this.ukladPlanszy = ukladPlanszy;
+            }
+            if (numer == 1) {
+                int[][] ukladPlanszy = {{10, 10, 10, 10, 10, 10, 10},
+                                        {10, 1, 10, 10, 10, 10, 10},
+                                        {10, 10, 10, 10, 10, 2, 10},
+                                        {10, 10, 10, 10, 10, 10, 10},
+                                        {10, 10, 10, 10, 10, 10, 10},
+                                        {10, 4, 10, 10, 10, 7, 10},
+                                        {10, 10, 2, 10, 10, 10, 10}};
+                nrPlanszy = numer + 2;
+                this.ukladPlanszy = ukladPlanszy;
+            }
         }
         else if (poziomTrudnosci == 3){
-            ukladPlanszy = new int[9][9];
+            if (numer == 0) {
+                int[][] ukladPlanszy = {{10, 10, 2, 10, 10, 10, 10, 2, 10, 10},
+                                        {10, 2, 10, 10, 1, 10, 10, 10, 2, 10},
+                                        {10, 10, 10, 2, 10, 10, 1, 10, 10, 10},
+                                        {10, 10, 10, 10, 10, 2, 10, 10, 10, 10},
+                                        {10, 10, 10, 10, 10, 10, 10, 10, 10, 10},
+                                        {10, 10, 10, 10, 10, 10, 10, 10, 10, 10},
+                                        {10, 10, 10, 10, 10, 10, 10, 10, 10, 10},
+                                        {10, 10, 10, 1, 10, 10, 6, 10, 10, 10},
+                                        {10, 5, 10, 10, 10, 10, 10, 10, 6, 10},
+                                        {10, 10, 2, 10, 10, 10, 10, 4, 10, 10}};
+                nrPlanszy = numer + 4;
+                this.ukladPlanszy = ukladPlanszy;
+            }
+            if (numer == 1) {
+                int[][] ukladPlanszy = {{10, 10, 1, 10, 10, 10, 10, 10, 10, 10},
+                                        {10, 2, 10, 1, 10, 10, 10, 10, 10, 3},
+                                        {10, 10, 10, 10, 10, 10, 10, 10, 5, 10},
+                                        {10, 10, 10, 10, 10, 2, 10, 10, 10, 10},
+                                        {10, 10, 10, 10, 10, 10, 10, 10, 10, 10},
+                                        {10, 10, 10, 10, 10, 10, 10, 10, 10, 10},
+                                        {10, 10, 10, 10, 10, 4, 10, 10, 10, 10},
+                                        {10, 10, 10, 10, 10, 10, 10, 10, 6, 10},
+                                        {10, 5, 10, 6, 10, 10, 10, 10, 10, 6},
+                                        {10, 10, 1, 10, 10, 10, 10, 10, 10, 10}};
+                nrPlanszy = numer + 4;
+                this.ukladPlanszy = ukladPlanszy;
+            }
         }
-
-        //do zrobienia!
+    }
+    public static void main(String[] args){
+        Gra gra = new Gra();
+        gra.losujUklad(1);
+        for (int i = 0; i < 5; i++){
+            System.out.println(".");
+            for (int x = 0; x < 5; x++){
+                System.out.println(gra.ukladPlanszy[i][x]);
+            }
+        }
     }
 }
